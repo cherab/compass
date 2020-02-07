3a36d7ce2e3833b0b629770bd2d150d25a5b5501
MatejTomes <tomes@ipp.cas.cz>
2020-02-07 11:36:24 +0100

    change setup.py to be more cherab-core like

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
 import sys
 import numpy
 import os
 import os.path as path
 import multiprocessing
 
-threads = multiprocessing.cpu_count()
+use_cython = True
 force = False
 profile = False
+line_profile = False
+install_rates = False
+
+if "--skip-cython" in sys.argv:
+    use_cython = False
+    del sys.argv[sys.argv.index("--skip-cython")]
 
 if "--force" in sys.argv:
     force = True
@@ -18,24 +23,58 @@ if "--profile" in sys.argv:
     profile = True
     del sys.argv[sys.argv.index("--profile")]
 
-compilation_includes = [".", numpy.get_include()]
+if "--line-profile" in sys.argv:
+    line_profile = True
+    del sys.argv[sys.argv.index("--line-profile")]
 
+if "--install-rates" in sys.argv:
+    install_rates = True
+    del sys.argv[sys.argv.index("--install-rates")]
+
+source_paths = ['cherab', 'demos']
+compilation_includes = [".", numpy.get_include()]
+compilation_args = []
+cython_directives = {
+    'language_level': 3
+}
 setup_path = path.dirname(path.abspath(__file__))
 
-# build extension list
-extensions = []
-for root, dirs, files in os.walk(setup_path):
-    for file in files:
-        if path.splitext(file)[1] == ".pyx":
-            pyx_file = path.relpath(path.join(root, file), setup_path)
-            module = path.splitext(pyx_file)[0].replace("/", ".")
-            extensions.append(Extension(module, [pyx_file], include_dirs=compilation_includes),)
+if line_profile:
+    compilation_args.append("-DCYTHON_TRACE=1")
+    compilation_args.append("-DCYTHON_TRACE_NOGIL=1")
+    cython_directives["linetrace"] = True
 
-if profile:
-    directives = {"profile": True}
+if use_cython:
+
+    from Cython.Build import cythonize
+
+    # build .pyx extension list
+    extensions = []
+    for package in source_paths:
+        for root, dirs, files in os.walk(path.join(setup_path, package)):
+            for file in files:
+                if path.splitext(file)[1] == ".pyx":
+                    pyx_file = path.relpath(path.join(root, file), setup_path)
+                    module = path.splitext(pyx_file)[0].replace("/", ".")
+                    extensions.append(Extension(module, [pyx_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
+
+    if profile:
+        cython_directives["profile"] = True
+
+    # generate .c files from .pyx
+    extensions = cythonize(extensions, nthreads=multiprocessing.cpu_count(), force=force, compiler_directives=cython_directives)
+
 else:
-    directives = {}
 
+    # build .c extension list
+    extensions = []
+    for package in source_paths:
+        for root, dirs, files in os.walk(path.join(setup_path, package)):
+            for file in files:
+                if path.splitext(file)[1] == ".c":
+                    c_file = path.relpath(path.join(root, file), setup_path)
+                    module = path.splitext(c_file)[0].replace("/", ".")
+                    extensions.append(Extension(module, [c_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
 
 setup(
     name="cherab-compass",
@@ -44,6 +83,6 @@ setup(
     namespace_packages=['cherab'],
     packages=find_packages(),
     include_package_data=True,
-    ext_modules=cythonize(extensions, nthreads=threads, force=force, compiler_directives=directives)
+    ext_modules=extensions
 )
 
